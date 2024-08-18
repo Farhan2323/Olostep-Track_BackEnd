b57313641bbac6d4ef8eb90040310c8c8878f015
Farhan2323 <89047823+Farhan2323@users.noreply.github.com>
2024-08-18 14:52:35 -0500

    commit 5
    MONOG

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import string
 from openai import OpenAI
 import json
 import os
@@ -6,13 +7,36 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import base64
 import requests
-
+from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 CORS(app)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-client = OpenAI()
+openAI_client = OpenAI()
+
+
+
+uri = "mongodb+srv://farhan2323fa:EeV0ORVTs0jbQ48m@cluster0.tsgib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+db = client['url_summary_db'] 
+collection = db["summaries"]
+doc = {'title' : 'test' , 'summary' : 'teset'}
+#collection.insert_one(doc)
+
 
 
 def load_json_schema(schema_file: str) -> dict:
@@ -32,10 +56,12 @@ def scraper():
     data = request.get_json()
     logger.info('date: ' + str(data))
     url = data['url']
+
+
     response = requests.get(url)
     content = response.text
     schema = load_json_schema('schema.json')
-    response = client.chat.completions.create(
+    response = openAI_client.chat.completions.create(
     model='gpt-4o-mini',
     response_format={"type": "json_object"},
     messages=[
@@ -53,15 +79,17 @@ def scraper():
     ],
     max_tokens=500,
 )
+   
+
     logger.info(response.choices[0].message.content)
     json_data = json.loads(response.choices[0].message.content)
     logger.info('js data: ' + str(json_data))
+    logger.info('TITLE HERE LOOK ' + str(json_data['title']))
+    collection.insert_one({'url': url, 'title': json_data['title'], 'summary': json_data['summary']})
+    logger.info("Title and summary successfully stored in MongoDB!")
+
+
     return json_data
-
-
-
-
-
 
 
 
